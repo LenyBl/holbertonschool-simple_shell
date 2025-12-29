3299debb9d3e6152f51fa360b43099babd381d3f
Kedia12 <kediaihogoza01@gmail.com>
2025-12-29 09:43:52 +0000

    feat: file for shell.h

--- simple_shell.c
+++ simple_shell.c
@@ -1,10 +1,47 @@
-#include "simple_shell.h"
+#include "shell.h"
 /**
- * main - Entry point for the simple shell program
+ * main - simple UNIX command interpreter
  *
- * Return: Always 0 (Success)
+ * Return: 0
  */
 int main(void)
 {
-	return (0);
+char *line = NULL;
+size_t len = 0;
+ssize_t read;
+pid_t pid;
+int status;
+while (1)
+{
+if (isatty(STDIN_FILENO))
+write(STDOUT_FILENO, "#cisfun$ ", 9);
+read = getline(&line, &len, stdin);
+if (read == -1)
+{
+free(line);
+exit(0);
 }
+if (line[read - 1] == '\n')
+line[read - 1] = '\0';
+pid = fork();
+if (pid == 0)
+{
+char *argv[] = {line, NULL};
+if (execve(line, argv, environ) == -1)
+{
+perror("./shell");
+exit(1);
+}
+}
+else if (pid > 0)
+{
+wait(&status);
+}
+else
+{
+perror("fork");
+}
+}
+return (0);
+}
+
